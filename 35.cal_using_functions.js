f676e1d7e1160efe1ee55a3250c199be85995832
Deepikaaripirala9 <238w1a05k0@vrsec.ac.in>
2026-07-02 16:42:28 +0530

    Day-01 Javascript

---
+++ 35.cal_using_functions.js
@@ -0,0 +1,22 @@
+function add(a, b) {
+    return a + b;
+}
+
+function sub(a, b) {
+    return a - b;
+}
+
+function mul(a, b) {
+    return a * b;
+}
+
+function div(a, b) {
+    return a / b;
+}
+
+let a = 20, b = 5;
+
+console.log("Addition:", add(a, b));
+console.log("Subtraction:", sub(a, b));
+console.log("Multiplication:", mul(a, b));
+console.log("Division:", div(a, b));
